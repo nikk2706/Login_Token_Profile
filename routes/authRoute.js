0191e24b943423fe90bb2bb07264df80300b8612
NikunjPatel <137515605+nikk2706@users.noreply.github.com>
2023-11-21 12:08:10 +0530

    Update authRoute.js

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const authController = require('../controllers/authController');
 const {auth_Token} = require('../middleware/authToken');
-const hashPasswordmiddleware = require('../middleware/Hashpassword');
+const hashPasswordMiddleware = require('../middleware/Hashpassword');
 const router = express.Router();
 
-router.post('/register', hashPasswordmiddleware, authController.registerUser);
+router.post('/register', hashPasswordMiddleware, authController.registerUser);
 router.post('/login', authController.loginUser);
 router.get('/usersdata', authController.getUsersData);
 router.get('/profile/:id', auth_Token, authController.getprofile);
